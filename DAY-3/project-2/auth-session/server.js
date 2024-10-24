2e82ca547ec1d74b48c1d9a84eb4c23af67cbfd7
youngbeen <pybju0@gmail.com>
2024-10-24 18:18:11 +0900

    주석 추가

--- DAY-3/project-2/auth-session/server.js
+++ DAY-3/project-2/auth-session/server.js
@@ -1,63 +1,69 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret: 'secret_keys',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false, httpOnly: true }
-}));
+app.use(
+  session({
+    secret: "secret_keys",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true },
+  })
+);
 
-app.use(express.static(path.join(__dirname, 'views')));
+// 정적 서빙
+app.use(express.static(path.join(__dirname, "views")));
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+// 로그인
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
-app.post('/login', (req, res) => {
+// 로그인 처리
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  if (username === 'youngbeen' && password === 'qwe123') {
-    req.session.user = { username, role: 'admin' };
-    res.redirect('/dashboard');
+  // 간단한 인증 로직(실제로는 데이터베이스 조회 필요함)
+  if (username === "youngbeen" && password === "qwe123") {
+    req.session.user = { username, role: "admin" };
+    res.redirect("/dashboard");
   } else {
-    res.status(401).send('인증실패');
+    res.status(401).send("인증실패");
   }
 });
 
 // 대시보드 라우트 수정
-app.get('/dashboard', (req, res) => {
+app.get("/dashboard", (req, res) => {
   if (req.session.user) {
-    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+    res.sendFile(path.join(__dirname, "views", "dashboard.html"));
   } else {
-    res.redirect('/login');
+    res.redirect("/login");
   }
 });
 
 // 사용자 API 수정
-app.get('/api/user', (req, res) => {
+app.get("/api/user", (req, res) => {
   if (req.session.user) {
     res.json({ username: req.session.user.username });
   } else {
-    res.status(401).json({ message: '로그인하세요!' });
+    res.status(401).json({ message: "로그인하세요!" });
   }
 });
 
 // 로그아웃
-app.get('/logout', (req, res) => {
-  req.session.destroy(err => {
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
     if (err) {
-      return res.status(500).send('로그아웃하는 중 오류 발생!!!');
+      return res.status(500).send("로그아웃하는 중 오류 발생!!!");
     }
-    res.clearCookie('connect.sid');
-    res.redirect('/login');
+    res.clearCookie("connect.sid");
+    res.redirect("/login");
   });
 });
 
